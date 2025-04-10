41c6859caa5f315d6e1861cac1220581cd03ed1f
gain <163835628+leegain1@users.noreply.github.com>
2025-04-10 22:06:32 +0900

    3주차 자기소개카드

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Introduce from "./Introduce";
+
+function App() {
+    return (
+        <Introduce name="이가인" age="21" hobby="야구보기" />
+    );
+}
+
+export default App;
