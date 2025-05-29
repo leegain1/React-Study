a20bcaebb6cf667fd5955d6faac6e90833c05746
gain <163835628+leegain1@users.noreply.github.com>
2025-05-29 16:32:01 +0900

    Update App.js

--- 5주차/src/App.js
+++ 5주차/src/App.js
@@ -2,9 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './Header';
 import Article from './pages/Article';
 import Articles from './pages/Articles';
-import Login from './pages/Login';
-import MyPage from './pages/MyPage';
-import NotFound from './pages/NotFound';
+
 
 const App = () => {
     return (
@@ -14,9 +12,7 @@ const App = () => {
                 <Route path="category/:category" element={<Articles />} />
                 <Route path="article/:id" element={<Article />} />
             </Route>
-            <Route path="/login" element={<Login />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="*" element={<NotFound />} />
+
         </Routes>
     );
 };
